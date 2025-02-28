2abc70526f53e613faf1abb230803c8a40f2da51
Deepnshu Sharma <deepanshu0859@gmail.com>
2025-02-28 19:20:05 +0530

    Category page Details handler function

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -53,3 +53,43 @@ exports.getAllCategorys = async (req,res)=>{
         })
     }
 }
+
+// categoryPageDetails
+exports.categoryPageDetails = async(req,res)=>{
+    try{
+        // get category id
+        const {categoryId} = req.body;
+        // get courses of specified category
+        const specifiedCategory = await Category.findById({_id:categoryId}).populate("courses").exec();
+        // validation
+        if(!specifiedCategory){
+            return res.status(404).json({
+                success:false,
+                message:"No courses found",
+            })
+        }
+        // get courses of different categories
+        const differentCategories = await Category.find({_id:{$ne:categoryId}}).populate("courses").exec();
+
+        // get top selling courses
+        const topSellingCourses = await Course.find({}).sort({studentsEnrolled:-1}).limit(10).exec();
+        // return response
+        return res.status(200).json({
+            success:true,
+            message:"Category page details fetched successfully",
+            data:{
+                specifiedCategory:specifiedCategory,
+                differentCategories:differentCategories,
+                topSellingCourses:topSellingCourses
+            }
+        });
+    }
+    catch(error){
+        console.log(error.message);
+        return res.status(500).json({
+            success:false,
+            message:"Error while fetching category page details",
+            error:error.message,
+        });
+    }
+}
