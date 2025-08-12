9f5b7375fc9bf42310d51b7e64831aea1c6a93d2
Deepnshu Sharma <deepanshu0859@gmail.com>
2025-08-12 15:47:52 +0530

    image url fix

--- client/src/components/core/Dashboard/MyProfile.jsx
+++ client/src/components/core/Dashboard/MyProfile.jsx
@@ -17,7 +17,7 @@ export default function MyProfile() {
       <div className="flex items-center justify-between rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
         <div className="flex items-center gap-x-4">
           <img
-            src={user?.image}
+            src={user?.image?.includes("api.dicebear.com") ? user.image.replace(/^\/+/, "") : user?.image}
             alt={`profile-${user?.firstName}`}
             className="aspect-square w-[78px] rounded-full object-cover"
           />
